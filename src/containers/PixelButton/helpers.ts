97a165b301077df119e417fdfe5228e9d1e929ba
loliallen <maxim.kasakin@yahoo.com>
2022-02-19 05:48:07 +0300

    moved from cra to nextjs2

--- src/containers/PixelButton/helpers.ts
+++
@@ -1,6 +0,0 @@
-export const hexToRgb = (hexValue: string) => {
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValue);
-    if (result)
-        return `${parseInt(result[1], 16)} ${parseInt(result[2], 16)} ${parseInt(result[3], 16)}`
-    return "0 0 0"
-}
